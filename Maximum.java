b7080846fa7ea2a948a20bf11dd6215299d1672d
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-19 15:34:07 +0530

    UC3: added maximum String finder method

--- Maximum.java
+++ Maximum.java
@@ -7,26 +7,26 @@ public class Maximum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter first float number:");
-		Float N1 = sc.nextFloat();
+		System.out.println("Enter first String:");
+		String N1 = sc.next();
 		
-		System.out.println("Enter 2nd float number:");
-		Float N2 = sc.nextFloat();
+		System.out.println("Enter 2nd String:");
+		String N2 = sc.next();
 		
-		System.out.println("Enter 3rd float number:");
-		Float N3 = sc.nextFloat();
+		System.out.println("Enter 3rd String:");
+		String N3 = sc.next();
 		
-		maximumFloat(N1, N2, N3);
+		maximumString(N1, N2, N3);
 		
 	}
 	
-	public static void maximumFloat(Float N1, Float N2, Float N3 ) {
-		Float max = N1;
+	public static void maximumString(String N1, String N2, String N3 ) {
+		String max = N1;
 		if(N2.compareTo(max) > 0)
 			max = N2;
 		if(N3.compareTo(max) > 0)
 			max = N3;
-		System.out.println("Maximum Float Number: " +max);
+		System.out.println("Maximum String: " +max);
 	}
 
 }
