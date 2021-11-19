fa9d88c72e6e965de0a73110ad5ce3329d0e21dd
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-19 15:48:23 +0530

    Refactored and created only 1 generic method for all data types

--- Maximum.java
+++ Maximum.java
@@ -7,26 +7,24 @@ public class Maximum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//Input Integers
 		System.out.println("Enter first integer:");
-		Integer N1 = sc.nextInt();
-		
+		Integer I1 = sc.nextInt();
 		System.out.println("Enter 2nd integer:");
-		Integer N2 = sc.nextInt();
-		
+		Integer I2 = sc.nextInt();
 		System.out.println("Enter 3rd integer:");
-		Integer N3 = sc.nextInt();
-		
-		maximumInteger(N1, N2, N3);
+		Integer I3 = sc.nextInt();
+		maximumGeneric(I1, I2, I3);
 		
 	}
 	
-	public static void maximumInteger(Integer N1, Integer N2, Integer N3 ) {
-		Integer max = N1;
+	public static <E extends Comparable<E>> void maximumGeneric(E N1, E N2, E N3 ) {
+		E max = N1;
 		if(N2.compareTo(max) > 0)
 			max = N2;
 		if(N3.compareTo(max) > 0)
 			max = N3;
-		System.out.println("Maximum integer: " +max);
+		System.out.println("Maximum: " +max);
 	}
 
 }
