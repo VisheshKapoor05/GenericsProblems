fdbf6e5f199ea6193ce24da956acf7e70b2c0ef6
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-19 15:30:07 +0530

    UC1: added maximum float number finder method

--- Maximum.java
+++ Maximum.java
@@ -7,26 +7,26 @@ public class Maximum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter first integer:");
-		Integer N1 = sc.nextInt();
+		System.out.println("Enter first float number:");
+		Float N1 = sc.nextFloat();
 		
-		System.out.println("Enter 2nd integer:");
-		Integer N2 = sc.nextInt();
+		System.out.println("Enter 2nd float number:");
+		Float N2 = sc.nextFloat();
 		
-		System.out.println("Enter 3rd integer:");
-		Integer N3 = sc.nextInt();
+		System.out.println("Enter 3rd float number:");
+		Float N3 = sc.nextFloat();
 		
-		maximumInteger(N1, N2, N3);
+		maximumFloat(N1, N2, N3);
 		
 	}
 	
-	public static void maximumInteger(Integer N1, Integer N2, Integer N3 ) {
-		Integer max = N1;
+	public static void maximumFloat(Float N1, Float N2, Float N3 ) {
+		Float max = N1;
 		if(N2.compareTo(max) > 0)
 			max = N2;
 		if(N3.compareTo(max) > 0)
 			max = N3;
-		System.out.println("Maximum integer: " +max);
+		System.out.println("Maximum Float Number: " +max);
 	}
 
 }
